25e4339d0bdad4d1f87b1508d1fee4cddfaa6f3c
随便 <42101994+zjm404@users.noreply.github.com>
2020-10-26 16:05:56 +0800

    Update GitTestDemo3.java

--- src/main/java/cn/zhu4wp/seckill/GitTestDemo3.java
+++ src/main/java/cn/zhu4wp/seckill/GitTestDemo3.java
@@ -3,5 +3,6 @@ package cn.zhu4wp.seckill;
 public class GitTestDemo3{
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("HELLO WORLD");
     }
 }
