b3c3c2a7bf869ac788878d03b4b5ba3d68f3306e
随便 <42101994+zjm404@users.noreply.github.com>
2020-10-26 16:17:27 +0800

    Delete GitTestDemo3.java

--- src/main/java/cn/zhu4wp/seckill/GitTestDemo3.java
+++
@@ -1,8 +0,0 @@
-package cn.zhu4wp.seckill;
-
-public class GitTestDemo3{
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("HELLO WORLD");
-    }
-}
